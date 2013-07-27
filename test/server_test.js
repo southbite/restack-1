de3ac7b1b262457d4ba826a95217ca0d00438726
Johann du Toit <exxonno@gmail.com>
2013-07-27 20:14:06 +0200

    Updated to use mocha for tests

--- test/server_test.js
+++ test/server_test.js
@@ -1,141 +1,85 @@
 var request = require('request');
 
-exports.testCreateAccount = function(done)
-{
-	try
-	{
-		throw 'test not implemented';
-		
-		done(null, 'testCreateAccount test passed');
-	}
-	catch(e)
-	{
-		done(null, 'testCreateAccount test failed: ' + e);
-	}
-}
+describe('server-test', function(){
 
-exports.testLogin = function(done)
-{
-	try
-	{
-		throw 'test not implemented';
-		
-		done(null, 'testLogin test passed');
-	}
-	catch(e)
-	{
-		done(null, 'testLogin test failed: ' + e);
-	}
-}
+	it('testCreateAccount', function(callback){
 
-exports.testCreateData = function(done)
-{
-	try
-	{
-		throw 'testCreateData not implemented';
-		
-		done(null, 'cache test passed');
-	}
-	catch(e)
-	{
-		done(null, 'testCreateData test failed: ' + e);
-	}
-}
+		assert.fail();
 
-exports.testGetData = function(done)
-{
-	try
-	{
-		throw 'test not implemented';
-		
-		done(null, 'testGetData test passed');
-	}
-	catch(e)
-	{
-		done(null, 'testGetData test failed: ' + e);
-	}
-}
+		callback();
 
-exports.testFindData = function(done)
-{
-	try
-	{
-		throw 'test not implemented';
-		
-		done(null, 'testFindData test passed');
-	}
-	catch(e)
-	{
-		done(null, 'testFindData test failed: ' + e);
-	}
-}
+	});
 
-exports.testUpdateData = function(done)
-{
-	try
-	{
-		throw 'test not implemented';
-		
-		done(null, 'testUpdateData test passed');
-	}
-	catch(e)
-	{
-		done(null, 'testUpdateData test failed: ' + e);
-	}
-}
+	it('testLogin', function(callback){
 
-exports.testDeleteData = function(done)
-{
-	try
-	{
-		throw 'test not implemented';
-		
-		done(null, 'testDeleteData test passed');
-	}
-	catch(e)
-	{
-		done(null, 'testDeleteData test failed: ' + e);
-	}
-}
+		assert.fail();
 
-exports.testCreateGuest = function(done)
-{
-	try
-	{
-		throw 'test not implemented';
-		
-		done(null, 'testCreateGuest test passed');
-	}
-	catch(e)
-	{
-		done(null, 'testCreateGuest test failed: ' + e);
-	}
-}
+		callback();
 
-exports.testGuestInsufficientPriviledges = function(done)
-{
-	try
-	{
-		throw 'test not implemented';
-		
-		done(null, 'testGuestInsufficientPriviledges test passed');
-	}
-	catch(e)
-	{
-		done(null, 'testGuestInsufficientPriviledges test failed: ' + e);
-	}
-}
+	});
 
-exports.testGetPermissions = function(done)
-{
-	try
-	{
-		throw 'test not implemented';
-		
-		done(null, 'testGetPermissions test passed');
-	}
-	catch(e)
-	{
-		done(null, 'testGetPermissions test failed: ' + e);
-	}
-}
+	it('testCreateData', function(callback){
+
+		assert.fail();
+
+		callback();
+
+	});
+
+	it('testGetData', function(callback){
+
+		assert.fail();
+
+		callback();
+
+	});
+
+	it('testFindData', function(callback){
+
+		assert.fail();
+
+		callback();
+
+	});
+
+	it('testUpdateData', function(callback){
+
+		assert.fail();
+
+		callback();
+
+	});
+
+	it('testDeleteData', function(callback){
+
+		assert.fail();
+
+		callback();
+
+	});
+
+	it('testCreateGuest', function(callback){
+
+		assert.fail();
+
+		callback();
+
+	});
+
+	it('testGuestInsufficientPriviledges', function(callback){
+
+		assert.fail();
+
+		callback();
+
+	});
+
+	it('testGetPermissions', function(callback){
+
+		assert.fail();
+
+		callback();
+
+	});
+
+});
